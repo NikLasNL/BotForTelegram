6f0d2024f4a4d2a8b7257cade728be928496d4fe
BeCeLiYPeLmEn <nikitafil.1234@mail.ru>
2017-09-15 15:16:14 +0400

    Bot start pack
    Nekit change this shit to what u did yesterday (14.09.2017)

---
+++ index.js
@@ -0,0 +1,42 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '445138642:AAFLiCODZoPEXR9mAznxd5cfWkJwcTuMvgc';
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/^\/start/, (msg, match) => {
+
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Привет");
+    bot.sendMessage(chatId, "Давай знакомится?");
+});
+bot.onText(/^(Давай|Го)$/, (msg, match) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Меня зовут NaNik, а тебя ?");
+});
+bot.onText(/^(А меня (.+)|Я (.+))$/, (msg, match) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Приятно познакомится,"+match[1]);
+});
+
+bot.onText(/^Ты (.+)$/, (msg, match) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Сам такой");
+});
+
+bot.onText(/^(Кто первый ?| Кто первый $)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Лада конечно");
+});
+bot.onText(/^(Как дела?|Как жизнь?)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Отлично , а у тебя ?");
+});
+bot.onText(/^(Отлично|Хорошо|Заебца|Охуенно)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Ну и отлично.");
+});
+bot.onText(/^(Плохо|Херово |Не очень|Так себе|Хуево)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "А почему?");
+});
